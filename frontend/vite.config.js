116f4ea8f515111947b68ced95073e0195914d06
Rishi2668 <rishi.pamar2004@gmail.com>
2025-03-02 03:12:13 +0530

    added changes in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,24 @@
-// frontend/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+  // frontend/vite.config.js
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+  import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'./',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    base:'./',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
-});
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
